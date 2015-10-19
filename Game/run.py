77d2704d39db715cff78d8812393a6c896608dc8
Penaz91 <penazarea@altervista.org>
2015-10-19 10:23:00 +0200

    Added Config, HWAcceleration support+Fullscreen

--- Game/run.py
+++ Game/run.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 import pygame
 from game import Game
+import configparser
 if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read("game.conf")
+    screensize = (int(config["Screen"]["screenwidth"]),
+                  int(config["Screen"]["screenheight"]))
+    fullscreen = config.getboolean("Screen", "fullscreen")
+    flags = None
+    if fullscreen:
+        flags = pygame.FULLSCREEN | pygame.HWACCEL
+    else:
+        flags = 0
     pygame.init()
-    screensize = (800, 600)
-    screen = pygame.display.set_mode(screensize)
+    screen = pygame.display.set_mode(screensize, flags)
     Game().main(screen)
