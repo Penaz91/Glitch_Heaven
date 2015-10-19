0dd0ea92a9fdb0fd4ecf41fbab325845619fbab8
Penaz91 <penazarea@altervista.org>
2015-10-19 18:39:41 +0200

    Sync Commit

--- Game/libs/particle.py
+++ Game/libs/particle.py
@@ -2,44 +2,48 @@
 # Part of the Glitch_Heaven Project
 # Copyright 2015 Penaz <penaz@altervista.org>
 import pygame
+import random
 
 
 class Particle (pygame.sprite.Sprite):
     def __init__(self, position, *groups):
         super(Particle, self).__init__(*groups)
-        self.age = 500
-        self.color = (255, 255, 255)
-        self.colorsteps = self.colorfade(self.color, (0, 0, 0), 100)
+        self.age = 10
+        self.color = (255, 0, 0)
+        self.colorsteps = self.colorfade(self.color, (0, 255, 0), 10)
         self.image = pygame.surface.Surface((3, 3))
         self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.rect.x = position[0]
-        self.rect.y = position[1]
+        self.rect.x = position[0] + random.randint(-5, 5)
+        self.rect.y = position[1] + random.randint(-5, 5)
 
     def update(self):
         self.age -= 1
         if self.age < 100:
             self.color = (
-                (self.color[0])-(self.colorsteps[0]),
-                (self.color[1])-(self.colorsteps[1]),
-                (self.color[2])-(self.colorsteps[2]))
+                (self.color[0])+(self.colorsteps[0]),
+                (self.color[1])+(self.colorsteps[1]),
+                (self.color[2])+(self.colorsteps[2]))
             self.image.fill(self.color)
         if self.age == 0:
             self.kill()
 
     def colorfade(self, startcolor, finalcolor, steps):
-        stepR = max(1, (finalcolor[0]-startcolor[0])/steps)
-        stepG = max(1, (finalcolor[1]-startcolor[1])/steps)
-        stepB = max(1, (finalcolor[2]-startcolor[2])/steps)
+        stepR = (finalcolor[0]-startcolor[0])/steps
+        stepG = (finalcolor[1]-startcolor[1])/steps
+        stepB = (finalcolor[2]-startcolor[2])/steps
         return (stepR, stepG, stepB)
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 group = pygame.sprite.Group()
 clock = pygame.time.Clock()
 while 1:
-    clock.tick(30)
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.MOUSEMOTION:
+            particle = Particle(pygame.mouse.get_pos(), group)
+            particle = Particle(pygame.mouse.get_pos(), group)
+            particle = Particle(pygame.mouse.get_pos(), group)
             particle = Particle(pygame.mouse.get_pos(), group)
     screen.fill((0, 0, 0))
     group.draw(screen)
