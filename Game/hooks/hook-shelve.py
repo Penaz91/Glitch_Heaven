4e079fb09a7a875f12251f0ad904de1d3a5bcf34
Penaz91 <penazarea@altervista.org>
2015-12-19 11:40:44 +0100

    Added Shelve PyInstaller Hook, fix Issue#12

---
+++ Game/hooks/hook-shelve.py
@@ -0,0 +1,5 @@
+# Shelve Hidden Imports Hook for Frozen Binaries
+# Part of the Glitch_Heaven project
+# Copyright 2015 Penaz <penazarea@altervista.org
+
+hiddenimports=["dbm.ndbm", "dbm.dumb", "dbm.gnu"]
