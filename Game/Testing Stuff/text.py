a2cbda0715b5d6c35470ce7d12a9df2cba9f26c2
Penaz91 <penazarea@altervista.org>
2016-01-10 21:51:54 +0100

    Added glitched text generator test

---
+++ Game/Testing Stuff/text.py
@@ -0,0 +1,28 @@
+import pygame
+import os
+pygame.init()
+screen = pygame.display.set_mode((800, 240))
+font = pygame.font.Font(os.path.join("..",
+                                     "resources", "fonts",
+                                     "TranscendsGames.otf"), 32)
+purple = font.render("Start the Main Campaign",
+                     False,
+                     (236, 0, 200)).convert_alpha()
+green = font.render("Start the Main Campaign",
+                    False,
+                    (0, 236, 5)).convert_alpha()
+white = font.render("Start the Main Campaign",
+                    False,
+                    (255, 255, 255)).convert_alpha()
+entire = pygame.surface.Surface((purple.get_width() + 5, purple.get_height()))
+entire.blit(purple, (0, 0))
+entire.blit(green, (4, 0))
+entire.blit(white, (2, 0))
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+    screen.blit(entire, (50, 120))
+    pygame.display.update()
+pygame.quit()
+quit()
