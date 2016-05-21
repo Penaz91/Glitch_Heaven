2bfc7304b578bde3042eb05240f9860dc508e104
Penaz91 <penazarea@altervista.org>
2016-05-21 11:39:43 +0200

    Some "if" cut

--- Game/loadmenu.py
+++ Game/loadmenu.py
@@ -19,7 +19,12 @@ class loadMenu(menu):
                             "resources", "fonts",
                             "TranscendsGames.otf"), 24)
         self.id = 0
-        self.oldid = -1
+        self.n, self.nl1, self.nl2, self.np1, self.np2 = 5*[None]
+        if self.dirlist is not None:
+            self.updateOperation = self.update_yes
+        else:
+            self.updateOperation = self.update_no
+        self.updateOperation()
         super().__init__(screen, keys, config, sounds, log)
 
     def makeMainMenuItem(self):
@@ -40,21 +45,28 @@ class loadMenu(menu):
     def makeMenuItems(self):
         self.makeMainMenuItem()
         
+    def update_no(self):
+        pass
+        
+    def update_yes(self):
+        self.n = self.font.render(self.dirlist[self.id], False, (255, 255, 255)).convert_alpha()
+        self.nl1 = self.font.render(self.dirlist[(self.id - 1) % len(self.dirlist)], False, (128, 128, 128)).convert_alpha()
+        self.nl2 = self.font.render(self.dirlist[(self.id - 2) % len(self.dirlist)], False, (64, 64, 64)).convert_alpha()
+        self.np1 = self.font.render(self.dirlist[(self.id + 1) % len(self.dirlist)], False, (128, 128, 128)).convert_alpha()
+        self.np2 = self.font.render(self.dirlist[(self.id + 2) % len(self.dirlist)], False, (64, 64, 64)).convert_alpha()
+        self.updateOperation = self.update_no
+        
     def doMoreLoopOperations(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             self.id = (self.id - 1) % len(self.dirlist)
+            self.updateOperation = self.update_yes
         if keys[pygame.K_RIGHT]:
             self.id = (self.id + 1) % len(self.dirlist)
+            self.updateOperation = self.update_yes
 
     def doAdditionalBlits(self):
-        if self.oldid != self.id and self.dirlist is not None:
-            self.n = self.font.render(self.dirlist[self.id], False, (255, 255, 255)).convert_alpha()
-            self.nl1 = self.font.render(self.dirlist[(self.id - 1) % len(self.dirlist)], False, (128, 128, 128)).convert_alpha()
-            self.nl2 = self.font.render(self.dirlist[(self.id - 2) % len(self.dirlist)], False, (64, 64, 64)).convert_alpha()
-            self.np1 = self.font.render(self.dirlist[(self.id + 1) % len(self.dirlist)], False, (128, 128, 128)).convert_alpha()
-            self.np2 = self.font.render(self.dirlist[(self.id + 2) % len(self.dirlist)], False, (64, 64, 64)).convert_alpha()
-            self.oldid = self.id
+        self.updateOperation()
         if self.dirlist is not None:
             self.screen.blit(self.nl2, (50, 260))
             self.screen.blit(self.nl1, (50, 290))
